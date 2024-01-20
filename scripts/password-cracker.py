f25013c2d1ef4af14037fc045c5e468d6bd0ce90
Suraj Neupane <71274266+VectorJamo@users.noreply.github.com>
2024-01-20 13:29:28 -0700

    Add files via upload

---
+++ scripts/password-cracker.py
@@ -0,0 +1,27 @@
+# Password cracker
+import random
+
+target = 'Aa3'
+small_letters = 'abcdefghijklmnopqrstuvwxyz'
+capital_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+domain = ''
+
+# First, check the contents of the password
+for c in target:
+    if c.islower():
+        domain += small_letters
+    elif c.isupper():
+        domain += capital_letters
+    elif c.isnumeric():
+        domain += numbers
+
+guess = ''
+# Then, crack the password
+while (guess != target):
+    guess = ''
+    for x in range(len(target)):
+        guess += domain[random.randint(0, len(domain)-1)]
+
+
+print("Your password is: ", guess)
